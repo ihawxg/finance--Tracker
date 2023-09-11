e936ff66bf66b19d27fee06a3aa8563e7f090a59
iHawx <ihawxg@gmail.com>
2023-09-11 13:06:15 +0300

    Model folder with classes for expense, model, user added, redux/store…

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+import { BrowserRouter } from "react-router-dom";
+import store from './store';
+
+
 
 ReactDOM.render(
-    <BrowserRouter>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-            <App/>
-        </DevSupport>
-    </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <Provider store={store}>
+       <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
