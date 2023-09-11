60ebc23948954ebe94e040bd02d83bb1dba1072e
iHawx <ihawxg@gmail.com>
2023-09-11 13:21:48 +0300

    setup for firebase

--- src/components/DatePick.js
+++ src/components/DatePick.js
@@ -1,23 +1,27 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import DateAdapter from '@mui/lab/AdapterMoment';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
-import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import React from "react";
 
-export default function BasicDatePicker(props) {
-  const [value, setValue] = useState(null);
-
-  return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DatePicker
-        label={props.label}
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-      />
-    </LocalizationProvider>
-  );
+export default function DatePick(props){
+    const [value, setValue] = React.useState(new Date());
+    const isDisabled = (typeof props.disabled != "undefined") ? props.disabled : false;
+    return (
+        <LocalizationProvider dateAdapter={DateAdapter}>
+            <DatePicker
+                disableFuture
+                disabled={isDisabled}
+                label="Birthdate"
+                openTo="year"
+                views={['year', 'month', 'day']}
+                value={value}
+                onChange={(newValue) => {
+                    const data = JSON.stringify(newValue["_d"]).slice(0,11);
+                    setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+            />
+        </LocalizationProvider>
+    );
 }
