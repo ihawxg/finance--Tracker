78ef54572ed22d1c60725582b7e71267bdb06301
iHawx <ihawxg@gmail.com>
2023-09-12 17:22:06 +0300

    added functionality for adding categories localy in redux and in firebase

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import { userReducer } from './reducers/userReducer';
-import { snackbarReducer } from './reducers/snackbarReducer'
+import { userReducer } from './redux/reducers/userReducer';
+import { snackbarReducer } from './redux/reducers/snackbarReducer'
 
 const composedEnhancer = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 const rootReducer = combineReducers({
---
+++ src/utils/consts.js
@@ -0,0 +1,20 @@
+export const basicExpenseCategories = [
+    "Food",
+    "Entertainment",
+    "Grocories",
+    "Rent",
+    "Insurance",
+    "Health",
+    "Entertainment",
+    "Transportation",
+    "Knowledge"
+];
+
+export const basicIncomeCategories = [
+    "Wage",
+    "Salary",
+    "Commision",
+    "Selling",
+    "Investment",
+    "Allowance"
+];
