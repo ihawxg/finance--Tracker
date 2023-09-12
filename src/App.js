c67f7b6d5f30fd818dc499f1850e355da345144c
iHawx <ihawxg@gmail.com>
2023-09-12 17:12:29 +0300

    added required pages and have set up the firebase with redux

--- src/App.js
+++ src/App.js
@@ -5,24 +5,35 @@ import AboutUsPage from "./components/AboutUsPage";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
 import ProfilePage from "./components/ProfilePage";
+import Snackbar from './components/Snackbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
+import CategoriesPage from "./components/CategoriesPage";
+import AccountsPage from "./components/AccountsPage";
+import BudgetsPage from "./components/BudgetsPage";
+import HistoryPage from "./components/HistoryPage";
+import ReportsPage from "./components/ReportsPage";
 
 function App() {
-  return (
-    <>
-        <Header />
-        <Snackbar/>
-        <Routes>
-            <Route path="/home" element={<HomePage />}/>
-            <Route path="/login" element={<LoginPage />}/>
-            <Route path="/register" element={<RegisterPage />}/>
-            <Route path="/about" element={<AboutUsPage />}/>
-            <Route path="/" element={<AboutUsPage />}/>
-            <Route path="/profile" element={<ProfilePage/>}/>
-        </Routes>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Snackbar />
+            <Routes>
+                <Route path="/home" element={<HomePage />}/>
+                <Route path="/login" element={<LoginPage />}/>
+                <Route path="/register" element={<RegisterPage />}/>
+                <Route path="/about" element={<AboutUsPage />}/>
+                <Route path="/" element={<AboutUsPage />}/>
+                <Route path="/profile" element={<ProfilePage/>}/>
+                <Route path="/categories" element={<CategoriesPage/>}/>
+                <Route path="/accounts" element={<AccountsPage/>}/>
+                <Route path="/budgets" element={<BudgetsPage/>}/>
+                <Route path="/history" element={<HistoryPage/>}/>
+                <Route path="/reports" element={<ReportsPage/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,68 +1,23 @@
 import { ADD_EXPENSE, ADD_GOAL, ADD_INCOME, ADD_BUDGET, CLEAR_GOALS, LOGIN, LOGOUT } from '../actions/userActions';
-import { ADD_EXPENSE, ADD_GOAL, ADD_INCOME, ADD_BUDGET, ADD_CATEGORY_INCOME, ADD_CATEGORY_EXPENSE, CLEAR_GOALS, LOGIN, LOGOUT } from '../actions/userActions';
 import { incomeArr } from '../mock-data/mock-income';
 import { expenseArr } from '../mock-data/mock-expense';
 import { budgetArr } from '../mock-data/mock-budget';
 import { account, category } from '../mock-data/mock-accounts-categories';
-import {basicIncomeCategories, basicExpenseCategories} from "../../utils/consts";
 
 const INITIAL_STATE = {
     logged: true,
     user : {
-        email: "vasko47@abv.bg",
-        accounts: [
-            {
-                name: "main",
-                budgets: [],
-                categories: [1,2,3],
-                expenses: [],
-                incomes: [
-                    {
-                        date: "2/18/2022",
-                        amount: "123",
-                        category: "Initial Deposit",
-                        description: "Initial App Deposit"
-                    }
-                ],
-                goals: []
-            },
-            {
-                name: "sub-zero",
-                budgets: [],
-                categories: ["entertainment", "clothes", "studies"],
-                expenses: [],
-                incomes: [
-                    {
-                        date: "2/18/2022",
-                        amount: "1200",
-                        category: "Initial Deposit",
-                        description: "Initial App Deposit"
-                    }
-                ],
-                goals: []
-            },
-            {
-                name: "schmain",
-                budgets: [],
-                categories: ["swimming", "books", "furniture"],
-                expenses: [],
-                incomes: [
-                    {
-                        date: "2/18/2022",
-                        amount: "10",
-                        category: "Initial Deposit",
-                        description: "Initial App Deposit"
-                    }
-                ],
-                goals: []
-            }
-        ],
-        incomeCategories: basicIncomeCategories,
-        expenseCategories: basicExpenseCategories,
-        birthdate: "2001-11-09",
-        firstName: "Васил",
-        lastName: "Любенов",
-        id: "tWLn6IPOCs2TlzFObTFA"
+        firstName : 'Viktoria',
+        lastName : "Slavkova",
+        email : 'vx.slavkova@gmail.com',
+        birthdate : '03/18/1994',
+        incomes : incomeArr,
+        expenses : expenseArr,
+        budgets : budgetArr,
+        goals : [{descr : "Laptop", amount : 800}],
+        accounts : account,
+        categories : category,
+        startBudget : 1200
     }
 }
 
@@ -114,23 +69,6 @@ export const userReducer = (state = INITIAL_STATE, action) => {
                 user : {
                     ...state.user,
                     goals : [...state.user.goals, action.payload]
-                }
-            }
-
-        case ADD_CATEGORY_INCOME :
-            return {
-                ...state,
-                user : {
-                    ...state.user,
-                    incomeCategories: [...state.user.incomeCategories, action.payload]
-                }
-            }
-        case ADD_CATEGORY_EXPENSE :
-            return {
-                ...state,
-                user : {
-                    ...state.user,
-                    expenseCategories: [...state.user.expenseCategories, action.payload]
                 }
             }
         case CLEAR_GOALS :
