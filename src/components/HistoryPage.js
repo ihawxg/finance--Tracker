ea1980d80e023731f4e5b7e11b49dff0b5a40262
iHawx <ihawxg@gmail.com>
2023-09-14 19:52:40 +0300

    work on redux and filters

--- src/components/HistoryPage.js
+++ src/components/HistoryPage.js
@@ -1,68 +1,103 @@
-import AddButtons from "./AddButtons";
-import styles from "./styles/pages.module.css"
-import { useSelector, useDispatch } from "react-redux";
-import SelectInput from "./SelectInput";
-import React from "react";
-import FormDialog from "./FormDialog";
-import { removeIncomeExpense } from "../redux/actions/userActions";
+import styles from "./styles/pages.module.css";
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import styled from 'styled-components';
+import CheckIcon from '@mui/icons-material/Check';
+import { ChartRangeFilter } from "./ChartFilters";
 
+export default function HistoryPage() {
 
-export default function HistoryPage(){
-    const user = useSelector(state => state.userData.user);
-    const [currentAccounts, setCurrenctAccount] = React.useState([]); 
+    const accounts = useSelector(state => state.userData.user.accounts);
+    const allCategories = useSelector(state => state.userData.user.categories)
+    const allIncomes = accounts.map(account => account.incomes);
+    const allExpenses = accounts.map(account => account.expenses);
+    const allBudgets = accounts.map(account => account.budgets);
+    const allGoals = accounts.map(account => account.goals);
+    const allTransactions = allIncomes.concat(allExpenses, allBudgets, allGoals);
 
-    const dispatch = useDispatch();
+    const rangeFilter = useSelector(state => state.filters.range);
+    const fromDateFilter = useSelector(state => state.filters.from_date);
+    const toDateFilter = useSelector(state => state.filters.to_date);
+    const categoryFilter = useSelector(state => state.filters.category);
+    const accountFilter = useSelector(state => state.filters.account);
 
-    const handleChange = (accountNames) => {
-        setCurrenctAccount(accountNames); 
+    const filteredTransactions = allTransactions.flat()
+        .filter(e => Number(e.amount) >= rangeFilter[0] && Number(e.amount) <= rangeFilter[1])
+        .filter(e => categoryFilter.indexOf(e.category) > -1)
+        .filter(e => accountFilter.indexOf(e.account) > -1)
+        
+
+    const getTime = (timeStamp) => {
+        const dateTime = JSON.stringify(timeStamp);
+        const dateArr = dateTime.split("T");
+        const date = dateArr[0].slice(1);
+        const time = dateArr[1].slice(0, 5);
+        return `${date} / ${time}`;
     }
-
-    let accounts = [];
-    currentAccounts.forEach(accName => {
-        user.accounts.forEach(acc => {
-            if(acc.name === accName){
-                accounts.push(acc);
-            }
-        });
-    })
-
-    const handleClick = (id, accountName, isExpense) => {
-        dispatch(removeIncomeExpense(user, id, accountName, isExpense));
+    const [description, setDescription] = useState("");
+    
+    const confirmChange = (date, descr) => {
+        // dispatch(updateItem(description))
+        console.log(date, descr)
+        setDescription("")
     }
-
+    const handleDelete = (ident) => {
+        // dispatch(deleteItem(ident))
+        console.log(ident)
+    }
     return (
+
         <div className={styles.page}>
-         <h1>HistoryPage</h1>
-         <AddButtons/>
+            <ChartRangeFilter/>
+            <h1>HistoryPage</h1>
+            <table>
+            <thead>
+                <tr>
+                    <th>Amount</th>
+                    <th>Type</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th>Date/Time</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
 
-         <SelectInput handleChange={handleChange} accounts={user.accounts}/>
-         <div>
-             {accounts.map(acc => {
-                return acc.expenses.map(exp => {
-                     return (
-                        <>
-                            <h1>Expense: {exp.description} {exp.amount}</h1>
-                            <FormDialog operation="edit" value="Expense" prevAccountName={acc.name} expenseID={exp.id}/>
-                            <button onClick={() => handleClick(exp.id, acc.name, true)}> Remove </button>
-                        </>
-                     ); 
-                 })
-                 
-             })}
-
-            {accounts.map(acc => {
-                return acc.incomes.map(inc => {
-                    return (
-                        <>
-                            <h1>Incomes: {inc.description} {inc.amount}</h1>
-                            <FormDialog operation="edit" value="Income" prevAccountName={acc.name} incomeID={inc.id}/>
-                            <button onClick={() => handleClick(inc.id, acc.name, false)}> Remove </button>
-                        </>
-                    );
-                })
-             })}
-
-         </div>
-        </div>
+           
+                {filteredTransactions.map(item => {
+                        return (
+                            <tr key={item.date}>
+                                <td>{item.amount}</td>
+                                <td>{item.category.type}</td>
+                                <td>{item.category.name}</td>
+                                <td><StyledInput 
+                                        name="description"
+                                        defaultValue={item.descr} 
+                                        onKeyDown={(e) => {
+                                            if(e.key === 'Enter') {
+                                                confirmChange(item.date, description)
+                                            }
+                                        }}
+                                        onChange={(e) => setDescription(e.target.value)} />
+                                </td>
+                                <td>{getTime(item.date)}</td>
+                                <td><DeleteForeverIcon onClick={() => console.log(item.date)}/></td>
+                            </tr>
+                        )            
+                    }) 
+                    }
+                     </tbody>    
+                </table>
+        </div >
     );
 }
+
+
+const StyledInput = styled.input`
+    border : none;
+    outline : none;
+    background-color: transparent;
+    width: 400px;
+
+`
--- src/components/chartFilters.js
+++ src/components/chartFilters.js
@@ -1,12 +1,13 @@
 import React, { useState, useCallback } from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider'
-import { useSelector } from 'react-redux';
-import delay from '../util';
+import { useDispatch, useSelector } from 'react-redux';
+import delay from './../util';
+import { applyRangeFilter } from '../redux/actions/filtersActions';
 
 export const ChartFromFilter = () => {
     //from a date
-}
+} 
 
 export const ChartToFilter = () => {
     //to a date
@@ -21,38 +22,39 @@ export const ChartRangeFilter = () => {
 
     const [filteredIncomes, setFilteredIncomes] = useState([]);
     const [filteredExpenses, setFilteredExpenses] = useState([]);
-
+    const dispatch = useDispatch();
+    
     function valuetext(value) {
         return `BGN ${value}`;
     }
-    let incomes = useSelector(state => state.userData.user.incomes)
-    let expenses = useSelector(state => state.userData.user.expenses)
-    let amountsIncome = incomes.map( e => Number(e.amount));
-    let amountsExpense = expenses.map(e => Number(e.amount));
+    let incomes = useSelector(state => state.userData.user.accounts.map(account => account.incomes));
+    let expenses = useSelector(state => state.userData.user.accounts.map(account => account.expenses));
+    let amountsIncome = incomes.flat().map( e => Number(e.amount));
+    let amountsExpense = expenses.flat().map(e => Number(e.amount));
     const maxI = Math.max(...amountsIncome);
     const maxE = Math.max(...amountsExpense);
     let max = Math.max(maxI, maxE);
-
+    
     const [value, setValue] = useState([0, max]);
     //a step behing in console log? to be fixed
     const handleChange = (event, newValue) => {
-        setValue(newValue);
-        let tempArr = incomes.filter(obj => obj.amount >= newValue[0] && obj.amount <= newValue[1]);
-        console.log(newValue[0], newValue[1])
-        setFilteredIncomes([...tempArr])
-        console.log(filteredIncomes);
-    }
-    const delayedChange = delay(handleChange, 500);
-    return (
-        <Box sx={{ width: 300 }}>
-            <Slider
-                getAriaLabel={() => 'Amount range'}
-                value={value}
-                onChange={delayedChange}
-                valueLabelDisplay="auto"
-                max={max}
-                getAriaValueText={valuetext}
-            />
-        </Box>
-    );
+            setValue(newValue);
+            let tempArr = incomes.filter(obj => obj.amount >= newValue[0] && obj.amount <= newValue[1]);
+            console.log(newValue[0], newValue[1])
+            setFilteredIncomes([...tempArr])
+            dispatch(applyRangeFilter(newValue[0], newValue[1]));
+        }
+   const delayedChange = delay(handleChange, 500);
+  return (
+    <Box sx={{ width: 300 }}>
+      <Slider
+        getAriaLabel={() => 'Amount range'}
+        value={value}
+        onChange={delayedChange}
+        valueLabelDisplay="auto"
+        max={max}
+        getAriaValueText={valuetext}
+      />
+    </Box>
+  );
 }
