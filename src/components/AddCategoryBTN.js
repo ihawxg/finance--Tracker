1053fdda28b83fe7e8fe9f66e3a805e88702f624
iHawx <ihawxg@gmail.com>
2023-09-15 19:35:51 +0300

    added user editable color picking for categories

--- src/components/AddCategoryBTN.js
+++ src/components/AddCategoryBTN.js
@@ -10,96 +10,103 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Radio from '@mui/material/Radio';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateUserIncomeCategories, updateUserExpenseCategories, editExpenseCategories, editIncomeCategories } from '../redux/actions/userActions';
+import { updateUserIncomeCategories, updateUserExpenseCategories, editExpenseCategories, editIncomeCategories } from '../redux/actions/userActions'; 
+import { HexColorPicker } from "react-colorful";
+import styles from "./styles/pages.module.css";
 
 export default function AddCategoryBTN(props) {
-    const [open, setOpen] = React.useState(false);
-    const [categoryInfo, setCategoryInfo] = React.useState({name: "", type: "expense"});
-    const [value, setValue] = React.useState('expense');
+  const [open, setOpen] = React.useState(false);
+  const [categoryInfo, setCategoryInfo] = React.useState({name: "", type: "expense"});
+  const [value, setValue] = React.useState('expense');
+  const [color, setColor] = React.useState("#fff");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const user = useSelector(state => state.userData.user);
+  const user = useSelector(state => state.userData.user);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleAdd = () => {
-        if(props.operation !== "edit"){
-            if(value === "income") {
-                dispatch(updateUserIncomeCategories(user.id, user.incomeCategories, user.categories, categoryInfo));
-            }
-            else {
-                dispatch(updateUserExpenseCategories(user.id, user.expenseCategories, user.categories, categoryInfo));
-            }
-        }
-        else{
-            if(value === "expense") {
-                dispatch(editExpenseCategories(user.id, props.position, user.expenseCategories, user.incomeCategories, user.categories, user.categories[props.position], categoryInfo));
-            }
-            else {
-                dispatch(editIncomeCategories(user.id, props.position, user.expenseCategories, user.incomeCategories, user.categories, user.categories[props.position], categoryInfo));
-            }
-        }
-
-        setCategoryInfo({name: "", type: "expense"});
-        setOpen(false);
+  const handleAdd = () => {
+    if(props.operation !== "edit"){
+      if(value === "income") {
+          dispatch(updateUserIncomeCategories(user.id, user.incomeCategories, user.categories, categoryInfo, color));
+      } 
+      else {
+          dispatch(updateUserExpenseCategories(user.id, user.expenseCategories, user.categories, categoryInfo, color));
+      }
+    }
+    else{
+      if(value === "expense") {
+        dispatch(editExpenseCategories(user.id, props.position, user.expenseCategories, user.incomeCategories, user.categories, user.categories[props.position], categoryInfo, false, color));
+      }
+      else {
+        dispatch(editIncomeCategories(user.id, props.position, user.expenseCategories, user.incomeCategories, user.categories, user.categories[props.position], categoryInfo, false, color));
+      }
     }
 
-    const handleChangeText = (ev) => {
-        setCategoryInfo(prevInfo => ({...prevInfo, [ev.target.name]: ev.target.value}));
-    }
+    setCategoryInfo({name: "", type: "expense"});
+    setColor("#fff");
+    setOpen(false);
+  }
 
-    const handleRadioChange = (event) => {
-        setValue(event.target.value);
-        setCategoryInfo(prevInfo => ({...prevInfo, type: event.target.value}));
-    };
+  const handleChangeText = (ev) => {
+    setCategoryInfo(prevInfo => ({...prevInfo, [ev.target.name]: ev.target.value}));
+  }
 
-    return (
-        <div>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                {props.operation === "edit" ? "Edit" : "Add"} Category
-            </Button>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>{props.operation === "edit" ? "Edit" : "Add"} Category</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        {props.operation === "edit" ? "Edit category name or type" : "Enter category name and type!" }
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="categoryName"
-                        label="Category Name"
-                        name="name"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                        value={categoryInfo.name}
-                        onChange={handleChangeText}
-                    />
+  const handleRadioChange = (event) => {
+    setValue(event.target.value);
+    setCategoryInfo(prevInfo => ({...prevInfo, type: event.target.value}));
+  };
 
-                    <RadioGroup
-                        aria-labelledby="category-type"
-                        name="type"
-                        value={value}
-                        onChange={handleRadioChange}>
-                        <FormControlLabel value="income" control={<Radio />} label="Income" />
-                        <FormControlLabel value="expense" control={<Radio />} label="Expense" />
-                    </RadioGroup>
-
-
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}> Cancel </Button>
-                    <Button onClick={handleAdd} disabled={!categoryInfo.name}> {props.operation === "edit" ? "Edit Category" : "Add Category" } </Button>
-                </DialogActions>
-            </Dialog>
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        {props.operation === "edit" ? "Edit" : "Add"} Category
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{props.operation === "edit" ? "Edit" : "Add"} Category</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+          {props.operation === "edit" ? "Edit category name or type" : "Enter category name and type!" }
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="categoryName"
+            label="Category Name"
+            name="name"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={categoryInfo.name}
+            onChange={handleChangeText}
+          />
+    
+        <div className={styles.categoryContainer}>
+          <RadioGroup
+            aria-labelledby="category-type"
+            name="type"
+            value={value}
+            onChange={handleRadioChange}>
+              <FormControlLabel value="income" control={<Radio />} label="Income" />
+              <FormControlLabel value="expense" control={<Radio />} label="Expense" />
+          </RadioGroup>
+          
+          <HexColorPicker color={color} onChange={setColor}/>
         </div>
-    );
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}> Cancel </Button>
+          <Button onClick={handleAdd} disabled={!categoryInfo.name}> {props.operation === "edit" ? "Edit Category" : "Add Category" } </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
