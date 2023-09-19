7c2d04faedc689de3c8491c7ac034ed6d6ff4bcd
iHawx <ihawxg@gmail.com>
2023-09-19 20:20:33 +0300

    fixed some typos in components and unused code

--- src/components/FormDialog.js
+++ src/components/FormDialog.js
@@ -14,18 +14,17 @@ import { addBudget, addIncome, addExpense, editExpense, editIncome, editBudget, 
 import { useDispatch, useSelector } from 'react-redux';
 import { setSnackbar } from '../redux/actions/snackbarActions';
 import styles from "./styles/progress_card.module.css";
-import { getFormatedDate } from '../utils/util';
-import { DateRangeFilter } from './filters/DateRangeFilter';
-
+import { getFormatedDate } from '../utils/util'
+import { DateRangeFilter } from './filters/DateRangeFilter'
 export default function FormDialog(props) {
 
   const [open, setOpen] = useState(false);
-  const [amount, setAmount] = useState(null);
+  const [amount, setAmount] = useState(0);
   const [descr, setDescr] = useState("");
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(null);
   const [fromDate, setFromDate] = useState(getFormatedDate(new Date()));
   const [toDate, setToDate] = useState(getFormatedDate(new Date()));
-  const [dateRange, setDateRange] = useState([null, null]);
+  const [dateRange, setDateRange] = useState([null, null])
   const [category, setCategory] = useState("");
   const [account, setAccount] = useState("");
   const dispatch = useDispatch();
@@ -36,15 +35,15 @@ export default function FormDialog(props) {
   };
 
   const handleClose = () => {
-    setAmount(null);
+    setAmount(0);
     setDescr("");
     setAccount("");
     setCategory("");
     setFromDate(getFormatedDate(new Date()));
-    setSelectedDate(new Date());
+    setSelectedDate(null);
     setToDate(getFormatedDate(new Date()));
     setOpen(false);
-    setDateRange([null, null])
+    setDateRange([null, null]);
   };
 
   const handleAdd = (value) => {
@@ -89,9 +88,7 @@ export default function FormDialog(props) {
     handleClose();
   };
 
-  const handleEdit = (value) => {
-    dispatch(setSnackbar(true, "success", "Transaction updated!"))
-    
+  const handleEdit = (value) => {    
     const detail = {
       amount,
       descr,
@@ -121,7 +118,16 @@ export default function FormDialog(props) {
             from: JSON.stringify(new Date(dateRange[0])).replaceAll('"', ''), 
             to: JSON.stringify(new Date(dateRange[1])).replaceAll('"', '')
           }
-        
+          setAmount(0);
+          setDescr("");
+          setAccount("");
+          setCategory("");
+          setFromDate(getFormatedDate(new Date()));
+          setSelectedDate(null);
+          setToDate(getFormatedDate(new Date()));
+          setOpen(false);
+          setDateRange([null, null]);
+
           dispatch(editBudget(user, details));
         }
         break;
@@ -158,51 +164,46 @@ export default function FormDialog(props) {
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>{props.operation === "edit" ? "Edit" : "Add"} {props.value} </DialogTitle>
         <DialogContent>
-          <InputFields>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Amount"
+            type="number"
+            fullWidth
+            variant="standard"
+            name="amount"
+            value={amount}
+            onChange={handleInput}
+          />
           
-            <TextField
-              autoFocus
-              margin="dense"
-              sx={{width : '240px', marginBottom : props.value == 'Budget' || props.value == 'Savings' ? "15px" : "0"}}
-              id="name"
-              label="Amount"
-              type="number"
-              color="secondary"
-              variant="outlined"
-              name="amount"
-              value={amount}
-              onChange={handleInput}
-            />
-            
-              {(props.value === "Income" || props.value === "Expense") && <TextField
-              margin="dense"
-              id="name"
-              label="Description"
-              color="secondary"
-              type="text"
-              sx={{width : '240px'}}
-              variant="outlined"
-              name ="description"
-              value={descr}
-              inputProps={{ maxLength: 36 }}
-              onInput={handleInput}
-            />}
-           </InputFields>  
+            {(props.value === "Income" || props.value === "Expense") && <TextField
+            margin="dense"
+            id="name"
+            label="Description"
+            type="text"
+            fullWidth
+            variant="standard"
+            name ="description"
+            value={descr}
+            onInput={handleInput}
+          />}
+          
           <Pickers>
             <StyledEngineProvider injectFirst>
              
               {props.value === "Budget" ? (
-                <DateRangeFilter flow="column" disabled={false} value={dateRange} onChange={e => setDateRange(e)} />
-              ) : (props.value !== "Savings" && <BasicDatePicker value={selectedDate} selected={selectedDate} onChange={date => setSelectedDate(date)} />)}
-              {props.value !== "Budget" && <CategoryPicker fullWidth name="account" value={account} onChange={e => setAccount(e.target.value)} required/>}
-              {props.value !== "Savings" && <CategoryPicker fullWidth type={props.value} name="category" value={category} list={account} disabled={!((account && props.value !== "Budget") || props.value === "Budget")} onChange={e => setCategory(e.target.value)} />}
+                <DateRangeFilter disabled={false} value={dateRange} onChange={e => setDateRange(e)} />
+              ) : (props.value !== "Savings" && <BasicDatePicker label="Choose date" value={selectedDate} selected={selectedDate} onChange={date => setSelectedDate(date)} />)}
+              {props.value !== "Budget" && <CategoryPicker name="account" value={account} onChange={e => setAccount(e.target.value)} required/>}
+              {props.value !== "Savings" && <CategoryPicker type={props.value} name="category" value={category} list={account} disabled={!((account && props.value !== "Budget") || props.value === "Budget")} onChange={e => setCategory(e.target.value)} />}
             </StyledEngineProvider>
           </Pickers>
           
         </DialogContent>
         <DialogActions>
-          <Button color="secondary" fullWidth={true} onClick={handleClose}>Cancel</Button>
-          <Button color="secondary" fullWidth={true} variant="contained"
+          <Button fullWidth={true} onClick={handleClose}>Cancel</Button>
+          <Button fullWidth={true} variant="contained"
             disabled={!(((amount && category && account && descr && (selectedDate || (fromDate && toDate))) || ((amount && category && props.value === "Budget")) || ((amount && account && props.value === "Savings")) ))} 
             onClick={ props.operation === "edit" ? () => handleEdit(props.value) : () => handleAdd(props.value) }> {props.operation === "edit" ? "Edit" : "Add"} {props.value}
           </Button>
@@ -214,15 +215,9 @@ export default function FormDialog(props) {
 }
 
 const Pickers = styled.div`
-  gap: 15px;
-  width: 100%;
+  margin-top: 10px;
   display : flex;
-  flex-flow: column wrap;
-  align-items : center;
-  justify-content: flex-start;
-  margin-top:2px;
+  flex-flow: row wrap;
+  align-items : flex-start;
+  justify-content: space-between;
 `
-const InputFields = styled(Pickers)`
-  justify-content: flex-start;
-  gap: 5px;
-`;
